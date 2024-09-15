cfb94043a813f445f9f4107137b777f2882676c3
GhazaFadhlilbaqi <fadhlilbaqimuhammadghaza@gmail.com>
2024-09-15 18:52:25 +0700

    Fixed deploy.yml for new PWS Project link

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -124,3 +124,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://muhammad-ghaza31-mentalhealthtracker.pbp.cs.ui.ac.id", "https://muhammad-ghaza31-mentalhealthtracker.pbp.cs.ui.ac.id"]
+
