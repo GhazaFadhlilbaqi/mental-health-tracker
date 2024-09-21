48b57666d149f57734bac8cae7ca001631101428
GhazaFadhlilbaqi <fadhlilbaqimuhammadghaza@gmail.com>
2024-09-21 10:53:35 +0700

    Fixed Test Functions for Quiz

--- main/views.py
+++ main/views.py
@@ -20,7 +20,7 @@ def show_main(request):
         'name': request.user.username,
         'class': 'PBP KKI',
         'mood_entries': mood_entries,
-        'last_login': request.COOKIES['last_login'],
+        'last_login': request.COOKIES['last_login','Not Set'],
     }
 
     return render(request, "main.html", context)
@@ -71,16 +71,12 @@ def login_user(request):
 
       if form.is_valid():
             user = form.get_user()
-            login(request, user)
-            response = HttpResponseRedirect(reverse("main:show_main"))
-            response.set_cookie('last_login', str(datetime.datetime.now()))
-            return response
-      
             if user is not None:
                 login(request, user)
                 response = HttpResponseRedirect(reverse("main:show_main"))
                 response.set_cookie('last_login', str(datetime.datetime.now()))
                 return response
+      
    else:
       form = AuthenticationForm(request)
    context = {'form': form}
